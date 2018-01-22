0376818f818b6d1ca037669871ece16cdd8c6378
Uthaeus <Uth@github.com>
2018-01-21 21:11:54 -0700

    implemented nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :blogs
-  get 'pages/home'
 
   get 'pages/about'
 
   get 'pages/contact'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
